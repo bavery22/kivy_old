cf6037de6d330c4f3ed8155f060b549d37f46280
bavery <avery.brian@gmail.com>
2014-04-10 20:53:13 -0700

    hack. to be backed out l8r.

--- kivy/core/window/window_egl_klaatu.py
+++ kivy/core/window/window_egl_klaatu.py
@@ -16,7 +16,7 @@ class WindowEglKlaatu(WindowBase):
     def create_window(self):
         #bcm.host_init()
 
-        w, h = egl.get_display_size() #720, 1280 #bcm.graphics_get_display_size(0)
+        w, h = 1200,1920
         Logger.debug('Window: Actual display size: {}x{}'.format(
             w, h))
         self._size = w, h
